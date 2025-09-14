cf12a13f9da9a70f5b06c4c28b2cd801408e8e4b
Beetle <drquinsey@gmail.com>
2025-09-14 06:33:39 -0400

    add docs and make type clone

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+Contains a single type meant to allow for multi-threading of a prime-sieve. It would also work with any other sieve operating on an array of bools.
+
+
+## SyncUnsafeOnceCellFalsable
+
+Not a very catchy name but I figured it was better than my original name for it "WeirdBool".
+This is the primary type in the crate and has interior mutability given you have an immutable reference to it.
+
+Here's its documentation:
+
+> A `bool` that is initially always `true`. It may be set from `true` to `false`, but never the other way around. This makes it thread safe since it can only effectively be written to once.
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,33 @@
-#![feature(sync_unsafe_cell)]
-
-use std::cell::SyncUnsafeCell;
-
 /// A `bool` that is initially always true. It may be set from true to false, but never the other way around. This makes it thread safe since it can only effectively be written to once.
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct SyncUnsafeOnceCellFalsable {
-    inner: SyncUnsafeCell<bool>,
+    inner: bool,
 }
 
 impl SyncUnsafeOnceCellFalsable {
     #[must_use]
     pub const fn new() -> Self {
-        Self {
-            inner: SyncUnsafeCell::new(true),
-        }
+        Self { inner: true }
     }
 
-    /// Returns the `UnsafeCell` containing the `bool`.
-    pub const fn get(&self) -> &SyncUnsafeCell<bool> {
-        &self.inner
+    /// Returns a copy of the inner bool.
+    #[must_use]
+    pub const fn get(&self) -> bool {
+        self.inner
     }
 
-    /// Returns the inner `bool`.
-    pub const fn get_bool(self) -> bool {
-        self.inner.into_inner()
-    }
-
-    /**
-    Sets the bool to false.
-
-    ## Safety
-    This is thread safe and cannot cause race conditions given the behavior of this type.
-    */
-    #[allow(invalid_reference_casting)]
+    /// Sets the bool to false.
+    #[allow(invalid_reference_casting, clippy::missing_safety_doc)]
     pub const fn set_false(&self) {
-        // Dereference raw pointer to inner bool to make it false.
-        unsafe { *self.inner.get() = false };
+        // // Dereference raw pointer to inner bool to make it false.
+        // unsafe { *self.inner.get() = false };
+
+        let raw_ptr: *mut Self = std::ptr::from_ref::<Self>(self).cast_mut();
+        let mut_ref: &mut Self = unsafe { &mut *raw_ptr };
+        mut_ref.inner = false;
+
+        // The above but in one line
+        // unsafe { &mut *std::ptr::from_ref::<Self>(self).cast_mut() }.inner = false;
     }
 }
 
